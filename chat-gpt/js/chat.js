a815e43f9d9fb109301c1958ee2b7871df5127b3
zhangqd <zhangqd@digiwin.com>
2023-12-14 18:21:14 +0800

    feat: xhr和fetch

---
+++ chat-gpt/js/chat.js
@@ -0,0 +1,49 @@
+fetch('http://localhost:7010/chat', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    clear: true,
+  }),
+});
+
+// 获取form
+const form = document.querySelector('form');
+const textArea = document.querySelector('textarea');
+
+textArea.onkeydown = (e) => {
+  if (e.key === 'Enter' && !e.shiftKey && !e.ctrlKey && !e.altKey) {
+    e.preventDefault();
+    form.dispatchEvent(new Event('submit'));
+  }
+};
+
+form.onsubmit = async (e) => {
+  e.preventDefault();
+  const content = textArea.value;
+  createUserContent('我');
+  const robot = createRobotContent();
+  const resp = await fetch('http://localhost:7010/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content,
+    }),
+  });
+  // 流式读取
+  const reader = resp.body.getReader();
+  const decoder = new TextDecoder();
+  while (1) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    const txt = decoder.decode(value);
+    robot.append(txt);
+    document.documentElement.scrollTo(0, 1000000);
+  }
+  robot.over();
+};
