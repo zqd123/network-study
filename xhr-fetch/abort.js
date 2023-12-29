1a3a515d4843975a6615357878dff88ecea47a59
zhangqd <zhangqd@digiwin.com>
2023-12-29 12:26:54 +0800

    feat: fetch取消请求

--- xhr-fetch/abort.js
+++ xhr-fetch/abort.js
@@ -1,16 +1,20 @@
-const input = document.querySelector('input')
-const ul = document.querySelector('#ul')
-input.addEventListener('keyup', () => {
-    const value = input.value
-    console.log(value)
-    getSearchData(value)
-})
+const input = document.querySelector("input");
+const ul = document.querySelector("#ul");
+let controller;
+input.addEventListener("keyup", () => {
+  controller && controller.abort();// 存在就终止
+  controller = new AbortController();// new一个新的终止控制器
+  const value = input.value;
+  console.log(value);
+  getSearchData(value);
+  isFirst = false;
+});
 
-async function getSearchData(key){
-    const res = await fetch('http://localhost:3000/abort?key='+key)
-    const data = await res.json()
-    console.log(data);
-    ul.innerHTML = data.reduce((pre,cur)=> pre +=`<li>${cur}</li>`, '')
-    
+async function getSearchData(key) {
+  const res = await fetch("http://localhost:3000/abort?key=" + key, {
+    signal: controller.signal,
+  });
+  const data = await res.json();
+  console.log(data);
+  ul.innerHTML = data.reduce((pre, cur) => (pre += `<li>${cur}</li>`), "");
 }
-
