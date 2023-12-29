51edc9daa30a35f2ff847071561443c0d35893ce
zhangqd <zhangqd@digiwin.com>
2023-12-29 15:01:25 +0800

    feat: xhr 请求取消

--- xhr-fetch/abort.js
+++ xhr-fetch/abort.js
@@ -6,10 +6,14 @@ input.addEventListener("keyup", () => {
   controller = new AbortController();// new一个新的终止控制器
   const value = input.value;
   console.log(value);
-  getSearchData(value);
+  // getSearchData(value);
+  xhrGetSearchData(value)
   isFirst = false;
 });
-
+/**
+ * fetch 请求取消
+ * @param {string} key 
+ */
 async function getSearchData(key) {
   const res = await fetch("http://localhost:3000/abort?key=" + key, {
     signal: controller.signal,
@@ -17,4 +21,24 @@ async function getSearchData(key) {
   const data = await res.json();
   console.log(data);
   ul.innerHTML = data.reduce((pre, cur) => (pre += `<li>${cur}</li>`), "");
+}
+
+
+
+let xhr
+/**
+ * xhr 请求取消
+ * @param {string} key 
+ */
+function xhrGetSearchData(key) {
+  xhr && xhr.abort();// 存在就终止
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:3000/abort?key=" + key);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      console.log(data);
+    }
+  }
 }
