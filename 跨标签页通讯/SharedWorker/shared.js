09b1e51d982b3e874fc6210d04f55fa172abd7c1
zhangqd <zhangqd@digiwin.com>
2024-02-27 18:15:23 +0800

    sharedWorker调试阶段

---
+++ 跨标签页通讯/SharedWorker/shared.js
@@ -0,0 +1,11 @@
+const data = "";//存储用户发送的信息
+onconnect = (event) => {
+    const port = event.ports[0];//获取客户端端口
+    port.onmessage = (event) => {
+        if (event.data==='get') {
+            port.postMessage(data);//向客户端发送消息
+        }else{
+            data = event.data;//将用户发送的信息存储到data变量中
+        }
+    }
+}
